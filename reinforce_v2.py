b3ce7cc82e34cb3dc7f8670b24870992cb1a32aa
rl-michels <r.l.michels@protonmail.com>
2022-04-15 15:52:52 +0200

    Add files via upload

---
+++ reinforce_v2.py
@@ -0,0 +1,128 @@
+import gym
+
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from collections import deque
+
+
+class Reinforce:
+
+    def __init__(self, n_actions=2, n_nodes=[32, 16], learning_rate=0.05, gamma=1):
+        """
+        :param n_actions: Number of actions available to agent
+        :param n_nodes: Number of nodes in hidden layers
+        :param learning_rate: Learning rate of neural network
+        :param gamma: discount factor, used to calculate the targets during the update
+        """
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+        self.n_actions = n_actions
+        self.n_nodes = n_nodes
+        self.model = self.make_model()
+        self.optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+        
+
+
+    def make_model(self):
+        """Construct the Q network with input, hidden and output layers"""
+        
+        # Make input layer
+        inputs = layers.Input(shape=(4,))
+        layers_list = [inputs]
+        
+        # Make hidden layers
+        layers_list.append(layers.Dense(self.n_nodes[0], activation='relu')(layers_list[-1]))
+        layers_list.append(layers.Dense(self.n_nodes[1], activation='relu')(layers_list[-1]))
+        
+        # Make output layer
+        output = layers.Dense(self.n_actions, activation="softmax")(layers_list[-1])
+        
+        # Combine input and output layers to construct the network
+        model = keras.Model(inputs=inputs, outputs=output)
+
+        return model
+
+
+def reinforce(n_episodes=250, learning_rate=0.05, gamma=1, 
+               n_nodes=[32, 16], render=False):
+    ''' runs a single repetition of Reinforce
+    Return: rewards, a vector with the observed rewards at each timestep '''
+
+    agent = Reinforce(gamma=gamma, learning_rate=learning_rate)
+    
+    print(agent.model)
+    
+    reward_per_episode = []
+    
+    # Make the CartPole environment
+    env = gym.make("CartPole-v1")
+
+    grad = 0
+    
+    for i in range(n_episodes):
+        rewards = []
+        state = env.reset()
+        state = np.reshape(state, [1, 4])
+        episode = []
+        done = False
+        with tf.GradientTape() as tape:
+            while not done:
+                if render:
+                    env.render()
+
+                # Select action for given state and strategy
+                probabillities = agent.model(state)
+                action = np.random.choice([0,1], p=np.squeeze(probabillities))
+
+                # Simulate environment
+                next_state, reward, done, info = env.step(action)
+                next_state = np.reshape(next_state, [1, 4])
+                episode.append((action, reward, probabillities))
+                rewards.append(reward)
+                state = next_state
+
+
+            reward_per_episode.append(np.sum(rewards))
+
+            losses = []
+            R = 0
+            for action, reward, probabillities in episode[::-1]:
+                R = gamma * R + reward
+                loss = R * -tf.math.log(probabillities[0][action])
+                losses.insert(0, loss)
+            total_loss = sum(losses)
+            gradient = tape.gradient(total_loss, agent.model.trainable_weights)
+            agent.optimizer.apply_gradients(zip(gradient, agent.model.trainable_weights))
+
+
+        print("episode: ", i, " score: ", reward_per_episode[-1])
+            
+    env.close()
+    return reward_per_episode
+
+
+def test():
+    """Test function which prints the obtained rewards for the parameters below"""
+    
+    n_episodes = 500
+    gamma = 0.8
+    learning_rate = 0.01
+
+    # Hidden layers
+    n_nodes = [32, 16]
+
+    # Plotting parameters
+    render = False
+    rewards = reinforce(n_episodes=n_episodes,
+                         learning_rate=learning_rate,
+                         gamma=gamma,
+                         n_nodes=n_nodes,
+                         render=render)
+    print("Obtained rewards: {}".format(rewards))
+
+
+if __name__ == '__main__':
+    test()
