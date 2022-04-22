aa7de572f72bad5af2a1ce08e8cca2a325aeb481
rl-michels <r.l.michels@protonmail.com>
2022-04-22 11:28:24 +0200

    Add files via upload

--- actor_critic.py
+++ actor_critic.py
@@ -6,10 +6,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from collections import deque
-import time
 
 
-class Reinforce:
+class Actor_critic:
 
     def __init__(self, n_actions=2, n_nodes=[32, 16], learning_rate=0.05, gamma=1):
         """
@@ -39,21 +38,21 @@ class Reinforce:
         layers_list.append(layers.Dense(self.n_nodes[1], activation='relu')(layers_list[-1]))
         
         # Make output layer
-        output = layers.Dense(self.n_actions, activation="softmax")(layers_list[-1])
+        actor = layers.Dense(self.n_actions, activation="softmax")(layers_list[-1])
+        critic = layers.Dense(1, activation="linear")(layers_list[-1])
         
         # Combine input and output layers to construct the network
-        model = keras.Model(inputs=inputs, outputs=output)
+        model = keras.Model(inputs=inputs, outputs=[actor, critic])
 
         return model
 
 
-def reinforce(n_episodes=250, learning_rate=0.05, gamma=1,
-               n_nodes=[32, 16], baseline_subtraction = False, bootstrapping = False, bootstrapping_depth = 1,
-               render=False, print_episodes=False):
+def actor_critic(n_episodes=250, learning_rate=0.05, gamma=1,
+               n_nodes=[32, 16], baseline_subtraction=True, bootstrapping=True, bootstrapping_depth=1, render=False, print_episodes=False):
     ''' runs a single repetition of Reinforce
     Return: rewards, a vector with the observed rewards at each timestep '''
 
-    agent = Reinforce(gamma=gamma, learning_rate=learning_rate)
+    agent = Actor_critic(gamma=gamma, learning_rate=learning_rate)
 
     reward_per_episode = []
     
@@ -61,12 +60,15 @@ def reinforce(n_episodes=250, learning_rate=0.05, gamma=1,
     env = gym.make("CartPole-v1")
 
     grad = 0
-    
+
+
     for i in range(n_episodes):
         rewards = []
+        actions = []
+        probabilities = []
+        predictions = []
         state = env.reset()
         state = np.reshape(state, [1, 4])
-        episode = []
         done = False
         with tf.GradientTape() as tape:
             while not done:
@@ -74,26 +76,53 @@ def reinforce(n_episodes=250, learning_rate=0.05, gamma=1,
                     env.render()
 
                 # Select action for given state and strategy
-                probabillities = agent.model(state)
-                action = np.random.choice([0,1], p=probabillities.numpy()[0])
+                probs, prediction = agent.model(state)
+                #print(prediction, probabilities)
+                action = np.random.choice([0,1], p=probs.numpy()[0])
+
 
                 # Simulate environment
                 next_state, reward, done, info = env.step(action)
                 next_state = np.reshape(next_state, [1, 4])
-                episode.append((action, reward, probabillities))
+                #episode.append((action, reward, probs, prediction[0, 0]))
+                actions.append(action)
                 rewards.append(reward)
+                probabilities.append(probs)
+                predictions.append(prediction)
                 state = next_state
 
 
             reward_per_episode.append(np.sum(rewards))
 
-            losses = []
+
+            actor_losses = []
+            critic_losses = []
             R = 0
-            for action, reward, probabillities in episode[::-1]:
-                R = gamma * R + reward
-                loss = R * -tf.math.log(probabillities[0][action])
-                losses.insert(0, loss)
-            total_loss = sum(losses)
+            #for action, reward, probs, prediction in episode[::-1]:
+            for j in range(len(actions)-1, -1, -1):
+                action, reward, probs, prediction = actions[j], rewards[j], probabilities[j], predictions[j]
+
+                if bootstrapping:
+                    k = min(j + bootstrapping_depth, len(actions) - 1) - j # get the bootstrapping depth, minding the end of the episode
+                    if k == bootstrapping_depth: # if the episode is longer than the bootstrapping depth, then use estimated value
+                        R = np.sum([rewards[j+z]*(agent.gamma) ** z for z in range(k)])+predictions[j + k]*(agent.gamma ** k)
+                    else: # if the bootstrapping depth is longer than the rest of the episode, then use actual rewards.
+                        R = np.sum([rewards[j+z]*(agent.gamma) ** z for z in range(k)])
+
+                else:
+                    R = gamma * R + reward
+
+                if baseline_subtraction:
+                    loss = (R - prediction) * -tf.math.log(probs[0][action])
+
+                else:
+                    loss = R * -tf.math.log(probs[0][action])
+
+                actor_losses.append(loss)
+                #print(prediction, R)
+                critic_loss = tf.pow(prediction - R, 2)
+                critic_losses.append(critic_loss)
+            total_loss = sum(actor_losses) + sum((critic_losses))
             gradient = tape.gradient(total_loss, agent.model.trainable_weights)
             agent.optimizer.apply_gradients(zip(gradient, agent.model.trainable_weights))
 
@@ -107,25 +136,23 @@ def reinforce(n_episodes=250, learning_rate=0.05, gamma=1,
 def test():
     """Test function which prints the obtained rewards for the parameters below"""
     
-    n_episodes = 1000
+    n_episodes = 500
     gamma = 0.8
-    learning_rate = 0.01
+    learning_rate = 0.015
 
     # Hidden layers
     n_nodes = [32, 16]
 
     # Plotting parameters
     render = False
-    rewards = reinforce(n_episodes=n_episodes,
+    rewards = actor_critic(n_episodes=n_episodes,
                          learning_rate=learning_rate,
                          gamma=gamma,
                          n_nodes=n_nodes,
                          render=render,
-                        print_episodes=True)
+                         print_episodes=True)
     print("Obtained rewards: {}".format(rewards))
 
 
 if __name__ == '__main__':
-    now = time.time()
     test()
-    print('Running one iteration takes {} minutes'.format((time.time()-now)/60))
---
+++ experiment.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Practical for course 'Reinforcement Learning',
+Leiden University, The Netherlands
+2021
+By Thomas Moerland
+"""
+
+import numpy as np
+import time
+
+from reinforce import reinforce
+from actor_critic import actor_critic
+from Helper import LearningCurvePlot, smooth
+
+
+def average_over_repetitions(algorithm, n_repetitions,
+                             n_episodes=500, learning_rate=0.01, gamma=0.9, n_nodes=[64, 128],
+                             baseline_subtraction=True, bootstrapping=True, bootstrapping_depth=1,
+                             render=False, print_episodes=False, smoothing_window=51):
+    reward_results = np.empty([n_repetitions, n_episodes])  # Result array
+    now = time.time()
+    for rep in range(n_repetitions):  # Loop over repetitions
+        print("repetition: ", rep)
+        rewards = algorithm(n_episodes, learning_rate, gamma,
+                            n_nodes, baseline_subtraction, bootstrapping, bootstrapping_depth, render, print_episodes)
+        print("average reward obtained: ", np.mean(rewards))
+        reward_results[rep] = rewards
+
+    print('Running one setting takes {} minutes'.format((time.time() - now) / 60))
+    learning_curve = np.mean(reward_results, axis=0)  # average over repetitions
+    learning_curve = smooth(learning_curve, smoothing_window)  # additional smoothing
+    return learning_curve
+
+
+def experiment():
+    ####### Settings
+    # Experiment
+    n_repetitions = 10
+    smoothing_window = 51
+
+    n_episodes = 750
+    gamma = 0.8
+    learning_rate = 0.01
+
+    # Hidden layers
+    n_nodes = [32, 16]
+
+    algorithm = actor_critic
+
+    # actor critic parameters:
+    baseline_subtraction = True
+    bootstrapping = True
+    bootstrapping_depth = 1
+
+    # Plotting parameters
+    render = False
+    print_episodes = False
+
+    ####### Experiments
+
+    '''
+    # compare the performance of the different algorithms
+    Plot = LearningCurvePlot(title = 'Comparing algorithms')
+    algorithm = reinforce
+    learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+               learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping, bootstrapping_depth, render, smoothing_window)
+    Plot.add_curve(learning_curve,label="Reinforce")
+
+    algorithm = actor_critic
+    learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+               learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping, bootstrapping_depth, render, smoothing_window)
+    Plot.add_curve(learning_curve,label="Actor critic, no bootstrapping, no baseline subtraction")
+
+    baseline_subtraction = True
+    learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+               learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping, bootstrapping_depth, render, smoothing_window)
+    Plot.add_curve(learning_curve,label="Actor critic, no bootstrapping, with baseline subtraction")
+
+    baseline_subtraction = False
+    bootstrapping=True
+    learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+               learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping, bootstrapping_depth, render, smoothing_window)
+    Plot.add_curve(learning_curve,label="Actor critic, with bootstrapping, no baseline subtraction")
+
+    baseline_subtraction=True
+    learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+               learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping, bootstrapping_depth, render, smoothing_window)
+    Plot.add_curve(learning_curve,label="Actor critic, with bootstrapping and baseline subtraction")
+
+    Plot.save('comparing_algorithms.png')
+    '''
+    # Varying the bootstrapping depth
+    Plot = LearningCurvePlot(title='Bootstrapping depths')
+    # learning_rates = [0.1,0.05,0.01,0.005, 0.001]
+    depths = [1, 2, 3, 4, 5]
+    for depth in depths:
+        bootstrapping_depth = depth
+        learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+                                                  learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping,
+                                                  bootstrapping_depth, render, smoothing_window)
+        Plot.add_curve(learning_curve, label=r'depth = {} '.format(bootstrapping_depth))
+    Plot.save('bootstrap_depth={}.png'.format(depths))
+    bootstrapping_depth = 1
+
+    # varying Gamma:
+    Plot = LearningCurvePlot(title='Discount factor')
+
+    gammas = [1.1, 1.0, 0.9, 0.8, 0.7, 0.6]
+    for g in gammas:
+        gamma = g
+        learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+                                                  learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping,
+                                                  bootstrapping_depth, render, smoothing_window)
+        Plot.add_curve(learning_curve, label=r'$\gamma = {} '.format(gamma))
+    Plot.save('gamma={}.png'.format(depths))
+    gamma = 0.8
+    '''
+    # Varying the learning_rates
+    Plot = LearningCurvePlot(title = 'Actor Critic with baseline subtraction')
+    # learning_rates = [0.1,0.05,0.01,0.005, 0.001]
+    learning_rates = [0.03, 0.025, 0.02, 0.015, 0.01, 0.005, 0.001]
+    for learning_rate in learning_rates:
+        learning_curve = average_over_repetitions(algorithm, n_repetitions, n_episodes,
+               learning_rate, gamma, n_nodes, baseline_subtraction, bootstrapping, bootstrapping_depth, render, smoothing_window)
+        Plot.add_curve(learning_curve,label=r'$\alpha$ = {} '.format(learning_rate))
+    Plot.save('dqn_result_alpha={}.png'.format(learning_rates))
+    '''
+
+
+if __name__ == '__main__':
+    experiment()
